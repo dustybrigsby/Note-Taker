1baf3126887f9421422bc95ea469fd7f18ad87f3
Dusty Brigsby <dustybrigsby@gmail.com>
2023-07-20 23:23:18 -0700

    Update server.js
    updated PORT for heroku deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const notesData = require('./db/db.json');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.static('public'));
